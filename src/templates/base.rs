b2b3f9cdf7c55a64c3e396944ab000d976246a3f
Friedemann Sommer <social@friedemannsommer.com>
2022-05-07 17:32:11 +0200

    remove "maximum-scale" from viewport

--- src/templates/base.rs
+++ src/templates/base.rs
@@ -13,7 +13,7 @@ markup::define! {
             head {
                 meta["charset" = "UTF-8"];
                 meta["name" = "color-scheme", "content" = "dark light"];
-                meta["name" = "viewport", "content" = "width=device-width, initial-scale=1 , maximum-scale=1.0, user-scalable=1"];
+                meta["name" = "viewport", "content" = "width=device-width, initial-scale=1, user-scalable=1"];
                 title { "SearProxy" }
                 link["rel" = "icon", "type" = "image/png", "sizes" = "32x32", "href" = "./favicon-32x32.png"];
                 link["rel" = "icon", "type" = "image/png", "sizes" = "16x16", "href" = "./favicon-16x16.png"];
