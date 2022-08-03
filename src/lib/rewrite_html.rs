223d4e3348dc992d1a21624106e352cd954d1381
Friedemann Sommer <social@friedemannsommer.com>
2022-08-03 22:44:55 +0200

    updated `rewrite_body_n_1` test to accept the updated header wording

--- src/lib/rewrite_html.rs
+++ src/lib/rewrite_html.rs
@@ -849,8 +849,9 @@ mod tests {
         assert_eq!(
             std::str::from_utf8(rewriter.end().unwrap().html.as_slice()).unwrap(),
             // this is pretty finicky... (and will break if the "header.html" formatting changes)
-            "<body><div><h1><a href=\"./\" target=\"_self\" rel=\"noreferrer noopener\">SearProxy</a></h1><p>This is a proxified and sanitized version, visit \
-            <a href=\"https://www.example.com/index.html\" target=\"_self\" rel=\"noreferrer noopener\">original page</a>.</p></div><h1>Test</h1></body>"
+            "<body><div><h1><a href=\"./\" target=\"_self\" rel=\"noreferrer noopener\">SearProxy</a> is neither the owner nor the author of this content.</h1>\
+            <p>Scripts are deactivated. Web page appearance may have changed. Visit the <a href=\"https://www.example.com/index.html\" target=\"_self\" rel=\"noreferrer noopener\">\
+            original page</a>.</p></div><h1>Test</h1></body>"
         );
     }
 
