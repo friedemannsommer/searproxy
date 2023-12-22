9787b8b3125d7c89b7691963b17fe9fa8996f3d6
Friedemann Sommer <social@friedemannsommer.com>
2023-12-22 16:52:47 +0100

    fixed Docker container script to use the artifact directories

--- packaging/docker/copy-binary.sh
+++ packaging/docker/copy-binary.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 case "${TARGETPLATFORM}" in
-    "linux/arm64") TARGET_BINARY="aarch64-unknown-linux-musl" ;;
-    "linux/arm/v6") TARGET_BINARY="arm-unknown-linux-musleabihf" ;;
-    "linux/arm/v7") TARGET_BINARY="armv7-unknown-linux-musleabihf" ;;
-    "linux/amd64") TARGET_BINARY="x86_64-unknown-linux-musl" ;;
-    "linux/386") TARGET_BINARY="i686-unknown-linux-musl" ;;
+    "linux/arm64") ARTIFACT_DIR="SearProxy_aarch64-unknown-linux-musl" ;;
+    "linux/arm/v6") ARTIFACT_DIR="SearProxy_arm-unknown-linux-musleabihf" ;;
+    "linux/arm/v7") ARTIFACT_DIR="SearProxy_armv7-unknown-linux-musleabihf" ;;
+    "linux/amd64") ARTIFACT_DIR="SearProxy_x86_64-unknown-linux-musl" ;;
+    "linux/386") ARTIFACT_DIR="SearProxy_i686-unknown-linux-musl" ;;
     *) exit 1 ;;
 esac;
 
-cp "/opt/searproxy/binaries/$TARGET_BINARY" /usr/local/bin/searproxy;
+cp "/opt/searproxy/binaries/$ARTIFACT_DIR/searproxy" /usr/local/bin/searproxy;
